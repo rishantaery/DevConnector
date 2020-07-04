287c6bf691e0848fd5342f3f284f7f0a871b48d1
Rishant <rishant.aery@gmail.com>
2020-07-04 13:59:34 +0530

    4th june commit

---
+++ routes/api/profile.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route   POST api/profile
+// @desc    Create or update a user profile
+// @access  Private
+router.post('/', [auth], (req, res) => {});
+module.exports = router;
